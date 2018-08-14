1db40ee7d72a60aeb0282a5436dcbffcac9ccb9d
Brian Y Lin <me@brianl.in>
2018-08-14 13:00:35 -0700

    Move isotypes to choices.py

---
+++ explorer/choices.py
@@ -0,0 +1,25 @@
+ISOTYPES = (
+  ('Ala', 'Ala'),
+  ('Arg', 'Arg'),
+  ('Asn', 'Asn'),
+  ('Asp', 'Asp'),
+  ('Cys', 'Cys'),
+  ('Gln', 'Gln'),
+  ('Glu', 'Glu'),
+  ('Gly', 'Gly'),
+  ('His', 'His'),
+  ('Ile', 'Ile'),
+  ('iMet', 'iMet'),
+  ('Leu', 'Leu'),
+  ('Lys', 'Lys'),
+  ('Met', 'Met'),
+  ('Phe', 'Phe'),
+  ('Pro', 'Pro'),
+  ('Ser', 'Ser'),
+  ('SeC', 'SeC'),
+  ('Thr', 'Thr'),
+  ('Trp', 'Trp'),
+  ('Tyr', 'Tyr'),
+  ('Val', 'Val')
+)
+
--- explorer/forms.py
+++ explorer/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from . import models
+from . import choices
 
 CLADES = [(clade.taxid, '{} ({})'.format(clade.name, clade.rank)) for clade in models.Taxonomy.objects.all()]
 
@@ -7,7 +8,7 @@ class CompareForm(forms.Form):
   name = forms.CharField(max_length = 10, required = True)
   input_fasta = forms.CharField(widget = forms.Textarea)
   clade = forms.ChoiceField(choices = CLADES)
-  isotype = forms.ChoiceField(choices = models.ISOTYPES)
+  isotype = forms.ChoiceField(choices = choices.ISOTYPES)
 
   def is_valid(self):
     valid = super(SignInForm, self).is_valid()
@@ -20,5 +21,5 @@ class CompareForm(forms.Form):
       self._errors['no_clade'] = 'Invalid clade - does not exist'
       return False
 
-    if self.cleaned_data['isotype'] not in ['All'] + [i[0] for i in models.ISOTYPES]:
+    if self.cleaned_data['isotype'] not in ['All'] + [i[0] for i in choices.ISOTYPES]:
       self._errors['invalid_isotype'] = 'Invalid isotype'
