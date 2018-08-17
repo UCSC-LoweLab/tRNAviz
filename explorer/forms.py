f98ab429bd4e0dc1491e7f7ed9197535a0103589
Brian Y Lin <me@brianl.in>
2018-08-16 19:13:08 -0700

    Convert name, isotype, fasta fields to be generated by django

--- explorer/forms.py
+++ explorer/forms.py
@@ -6,10 +6,20 @@ from . import choices
 CLADES = [(clade.taxid, '{} ({})'.format(clade.name, clade.rank)) for clade in models.Taxonomy.objects.all()]
 
 class CompareForm(forms.Form):
-  name = forms.CharField(max_length = 20, required = False)
-  fasta = forms.CharField(widget = forms.Textarea, required = False)
+  name = forms.CharField(widget = forms.TextInput({
+    'class': 'form-control name-input',
+    'placeholder': 'Group name'
+    }), max_length = 20, required = False)
+  fasta = forms.CharField(widget = forms.Textarea({
+    'class': 'form-control fasta-input',
+    'placeholder': 'Paste tRNAs in FASTA format'
+    }), required = False)
   clade = forms.ChoiceField(choices = CLADES, required = False)
-  isotype = forms.ChoiceField(choices = choices.ISOTYPES, required = False)
+  isotype = forms.ChoiceField(
+    widget = forms.Select({'class': 'form-control multiselect isotype-select'}), 
+    initial = 'All',
+    choices = choices.ISOTYPES,
+    required = False)
   use_fasta = forms.BooleanField(widget = forms.CheckboxInput(attrs = {
     'class': 'use-fasta-toggle',
     'data-toggle': 'toggle',
@@ -25,11 +35,11 @@ class CompareForm(forms.Form):
     if not valid: return valid
 
     # validate clade
-    try:
-      clade = models.Taxonomy.objects.get(taxid = self.cleaned_data['clade'])
-    except models.Taxonomy.DoesNotExist:
-      self.add_error('clade', 'Invalid clade - does not exist in database')
-      return False
+    # try:
+    #   clade = models.Taxonomy.objects.get(taxid = self.cleaned_data['clade'])
+    # except models.Taxonomy.DoesNotExist:
+    #   self.add_error('clade', 'Invalid clade - does not exist in database')
+    #   return False
 
     return True
 
