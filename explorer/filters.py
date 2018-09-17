298525b30ee2a68fa5f8be9237ac3e7687d30a8b
Brian Y Lin <me@brianl.in>
2018-09-16 18:56:12 -0700

    Migrate to unified column names

--- explorer/filters.py
+++
@@ -1,7 +0,0 @@
-from . import models
-import django_filters
-
-class tRNAFilter(django_filters.FilterSet):
-    class Meta:
-        model = models.tRNA
-        fields = ['isotype', 'anticodon', 'genus', ]
