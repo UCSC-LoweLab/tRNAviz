ff4130a03059320e65dd00608d078108f2ed7654
Brian Y Lin <me@brianl.in>
2018-09-20 17:56:15 -0700

    Refactor, write passing compare services tests

--- explorer/compare.py
+++ explorer/compare.py
@@ -51,7 +51,12 @@ def bitchart(request, formset_json):
   trna_fasta_files = write_trnas_to_files(formset_data, seqs)
   ref_model_fh = build_reference_model(trna_fasta_files)
   ref_bits = calculate_normalizing_scores(ref_model_fh)
-  bits = align_trnas_collect_bit_scores(trna_fasta_files[1:], formset_data, ref_model_fh)
+
+  # Align tRNAs to reference model and collect bit scores
+  bits = pd.DataFrame()
+  for i, trna_fasta_fh in enumerate(trna_fasta_files[1:]):
+    current_bits = align_trnas_collect_bit_scores(trna_fasta_fh.name, formset_data[i + 2]['name'], ref_model_fh.name)
+    bits = bits.append(current_bits)
 
   # Normalize bits against reference bits
   bits['score'] = round(bits.apply(lambda x: x['score'] - ref_bits[ref_bits.position == x['position']]['score'].values[0], axis = 1), 2)
@@ -60,18 +65,11 @@ def bitchart(request, formset_json):
   ref_taxid = formset_data[0]['clade']
   ref_isotype = formset_data[0]['isotype']
   ref_cons = get_cons_bits(ref_taxid, ref_isotype)
-  ref_freqs = get_modal_bits(ref_taxid, ref_isotype)
+  ref_freqs = get_modal_freqs(ref_taxid, ref_isotype)
   bits = pd.concat([bits, ref_cons, ref_freqs], sort = True).reset_index(drop = True)
-  print(bits.head())
   
-  # Translate human readable codes to IUPAC codes and tooltip labels
-  bits['label'] = bits.feature.apply(lambda x: HUMAN_LABELS[x] if x in HUMAN_LABELS else x)
-  bits['feature'] = bits.feature.apply(lambda x: IUPAC_CODES[x] if x in IUPAC_CODES else x)
-
-  # Format data for d3
-  groups = ['Reference consensus', 'Most common feature'] + list(filter(lambda x: x not in ['Reference consensus', 'Most common feature'], bits.group.unique()))
-  bits = bits[bits.position.isin(BITCHART_POSITIONS)].to_dict(orient = 'index')
-  plot_data = {'bits': bits, 'groups': groups}
+  # Format data for visualization
+  plot_data = format_bits_for_viz(bits)
 
   return JsonResponse(plot_data, safe = False)
 
@@ -118,7 +116,6 @@ def write_trnas_to_files(formset_data, seqs):
     trna_fasta_files.append(trna_fasta_fh)
   return trna_fasta_files
 
-
 def build_reference_model(trna_fasta_files):
   # Build reference CM model
   ref_fasta = trna_fasta_files[0].name
@@ -157,48 +154,41 @@ def calculate_normalizing_scores(ref_model_fh):
   return ref_bits
 
 
-def align_trnas_collect_bit_scores(trna_fasta_files, formset_data, ref_model_fh):
-  # Align tRNAs to reference model and collect bit scores
-  bits = pd.DataFrame()
+def align_trnas_collect_bit_scores(trna_fasta, group_name, ref_model):
   num_model = '{}/euk-num.cm'.format(settings.ENGINE_DIR)
-  for i, trna_fasta_fh in enumerate(trna_fasta_files):
-    group_name = formset_data[i+2]['name']
+  num_model_align_fh = NamedTemporaryFile('r+')
+  processed_fasta_fh = NamedTemporaryFile('r+', buffering = 1)
+  parsetree_fh = NamedTemporaryFile('r+', buffering = 1)
 
-    num_model_align_fh = NamedTemporaryFile('r+')
-    processed_fasta_fh = NamedTemporaryFile('r+', buffering = 1)
-    parsetree_fh = NamedTemporaryFile('r+', buffering = 1)
+  # Remove introns from all tRNAs (except reference). First, align to numbering model to purge insertions
+  cmd_cmalign = 'cmalign -g --notrunc --matchonly -o {} {} {} > /dev/null'.format(num_model_align_fh.name, num_model, trna_fasta)
+  res = subprocess.run(cmd_cmalign, shell = True)
 
-    # Remove introns from all tRNAs (except reference). First, align to numbering model to purge insertions
-    cmd_cmalign = 'cmalign -g --notrunc --matchonly -o {} {} {} > /dev/null'.format(num_model_align_fh.name, num_model, trna_fasta_fh.name)
-    res = subprocess.run(cmd_cmalign, shell = True)
+  # remove introns using alignment and rewrite tRNAs to new file
+  for line in num_model_align_fh:
+    if line[0] in ['#', '/', '\n']: continue
+    seqname, seq = line.strip().split()
+    seq = seq.replace('-', '')
+    processed_fasta_fh.write('>{}\n{}\n'.format(seqname, seq))
 
-    # remove introns using alignment and rewrite tRNAs to new file
-    for line in num_model_align_fh:
-      if line[0] in ['#', '/', '\n']: continue
-      seqname, seq = line.strip().split()
-      seq = seq.replace('-', '')
-      processed_fasta_fh.write('>{}\n{}\n'.format(seqname, seq))
-
-    # realign to reference model and parse parsetree output
-    cmd_cmalign = 'cmalign -g --notrunc --matchonly --tfile {} -o /dev/null {} {} > /dev/null'.format(parsetree_fh.name, ref_model_fh.name, processed_fasta_fh.name)
-    res = subprocess.run(cmd_cmalign, shell = True)
-    current_bits = pd.DataFrame(parse_parsetree(parsetree_fh))
-    
-    # For selections with mutliple tRNAs, summarize by average score and modal feature
-    num_trnas = len(current_bits.seqname.unique())
-    modal_features = current_bits.groupby('position').apply(lambda x: x['feature'].mode()[0]).reset_index().rename(columns = {0: 'feature'})
-    current_bits = current_bits.set_index(['seqname', 'feature', 'position']).groupby('position').mean()
-    current_bits =  current_bits.join(modal_features.set_index('position')).reset_index()
-    current_bits = current_bits[['feature', 'position', 'score']]
-    current_bits['group'] = group_name
-    current_bits['total'] = num_trnas
-    bits = bits.append(current_bits)
-
+  # realign to reference model and parse parsetree output
+  cmd_cmalign = 'cmalign -g --notrunc --matchonly --tfile {} -o /dev/null {} {} > /dev/null'.format(parsetree_fh.name, ref_model, processed_fasta_fh.name)
+  res = subprocess.run(cmd_cmalign, shell = True)
+  bits = pd.DataFrame(parse_parsetree(parsetree_fh))
+  
+  # For selections with mutliple tRNAs, summarize by average score and modal feature
+  num_trnas = len(bits.seqname.unique())
+  modal_features = bits.groupby('position').apply(lambda x: x['feature'].mode()[0]).reset_index().rename(columns = {0: 'feature'})
+  bits = bits.set_index(['seqname', 'feature', 'position']).groupby('position').mean()
+  bits = bits.join(modal_features.set_index('position')).reset_index()
+  bits = bits[['feature', 'position', 'score']]
+  bits['group'] = group_name
+  bits['total'] = num_trnas
   return bits
 
 def get_cons_bits(ref_taxid, ref_isotype):
   ref_cons_qs = models.Consensus.objects.filter(taxid = ref_taxid, isotype = ref_isotype).values()
-  ref_cons = read_frame(ref_cons_qs).drop(['id', 'taxid', 'isotype'], axis = 1).stack().unstack(0).reset_index()
+  ref_cons = read_frame(ref_cons_qs).drop(['id', 'taxid', 'rank', 'isotype'], axis = 1).stack().unstack(0).reset_index()
   ref_cons.columns = ['position', 'feature']
   ref_cons.position = ref_cons.position.apply(lambda x: x[1:].replace('_', ':'))
   ref_cons['score'] = 0
@@ -206,7 +196,7 @@ def get_cons_bits(ref_taxid, ref_isotype):
   ref_cons['group'] = 'Reference consensus'
   return ref_cons
 
-def get_modal_bits(ref_taxid, ref_isotype):
+def get_modal_freqs(ref_taxid, ref_isotype):
   freqs_qs = models.Freq.objects.filter(taxid = ref_taxid, isotype = ref_isotype).values()
   ref_freqs = read_frame(freqs_qs).drop(['id', 'taxid', 'rank', 'isotype'], axis = 1)
   ref_freqs['feature'] = ref_freqs.drop(['position', 'total'], axis = 1).idxmax(axis = 1)
@@ -214,6 +204,18 @@ def get_modal_bits(ref_taxid, ref_isotype):
   ref_freqs['score'] = 0
   ref_freqs['group'] = 'Most common feature'
   return ref_freqs
+
+
+def format_bits_for_viz(bits):
+  # Translate human readable codes to IUPAC codes and tooltip labels
+  bits['label'] = bits.feature.apply(lambda x: HUMAN_LABELS[x] if x in HUMAN_LABELS else x)
+  bits['feature'] = bits.feature.apply(lambda x: IUPAC_CODES[x] if x in IUPAC_CODES else x)
+
+  # Format data for d3
+  groups = ['Reference consensus', 'Most common feature'] + list(filter(lambda x: x not in ['Reference consensus', 'Most common feature'], bits.group.unique()))
+  bits = bits[bits.position.isin(BITCHART_POSITIONS)].to_dict(orient = 'index')
+  plot_data = {'bits': bits, 'groups': groups}
+  return plot_data
 
 def parse_parsetree(parsetree_fh):
   positions = {4: '73', 5: '1:72', 6: '2:71', 7: '3:70', 8: '4:69', 9: '5:68', 10: '6:67', 11: '7:66', 12: '8', 13: '9', 18: '10:25', 19: '11:24', 20: '12:23', 21: '13:22', 22: '14', 23: '15', 24: '16', 25: '17', 26: '17a', 27: '18', 28: '19', 29: '20', 30: '20a', 31: '20b', 32: '21', 35: '26', 36: '27:43', 37: '28:42', 38: '29:41', 39: '30:40', 40: '31:39', 41: '32', 42: '33', 43: '34', 44: '35', 45: '36', 46: '37', 47: '38', 50: '44', 51: '45', 54: 'V11:V21', 55: 'V12:V22', 56: 'V13:V23', 57: 'V14:V24', 58: 'V15:V25', 59: 'V16:V26', 60: 'V17:V27', 61: 'V1', 62: 'V2', 63: 'V3', 64: 'V4', 65: 'V5', 68: '46', 69: '47', 70: '48', 71: '49:65', 72: '50:64', 73: '51:63', 74: '52:62', 75: '53:61', 76: '54', 77: '55', 78: '56', 79: '57', 80: '58', 81: '59', 82: '60'}
--- explorer/tests/test_compare.py
+++ explorer/tests/test_compare.py
@@ -1,33 +1,59 @@
 from django.test import TestCase, Client, tag, RequestFactory
 from django.urls import reverse
+from django.conf import settings
 
-from tempfile import NamedTemporaryFile
+import shutil
+from tempfile import NamedTemporaryFile, mkdtemp
 import json
+import pandas as pd
 from Bio import SeqIO
+import os
 
 from explorer import compare
 from explorer import forms
 
-
-@tag('current')
+@tag('compare')
 class CompareTests(TestCase):
   def setUp(self):
-    self.formset = forms.CompareFormSet({
+    self.ref_taxid = '4751'
+    self.ref_isotype = 'Asp'
+    self.factory = RequestFactory()
+    self.request = self.factory.get('')
+
+  @classmethod
+  def setUpClass(cls):
+    super(CompareTests, cls).setUpClass()
+    cls.formset = forms.CompareFormSet({
       'form-0-name': '', 'form-0-clade': '4751', 'form-0-isotype': 'Asp',
       'form-1-name': '', 'form-1-clade': '2759', 'form-1-isotype': 'All',
       'form-2-name': 'Test', 'form-2-clade': '4893', 'form-2-isotype': 'All',
       'form-TOTAL_FORMS': '3', 'form-MIN_NUM_FORMS': '0', 'form-MAX_NUM_FORMS': '1000', 'form-INITIAL_FORMS': '0'
       })
     
-    formset_json_fh = NamedTemporaryFile('w')
-    self.formset_json = formset_json_fh.name
-    self.formset_data = [form.as_dict() for form in self.formset]
-    formset_json_fh.write(json.dumps(self.formset_data))
-    formset_json_fh.close()
-    self.factory = RequestFactory()
-    self.request = self.factory.get(reverse('explorer:bitchart', kwargs = {'formset_json': self.formset_json}))
-    self.seqs = compare.read_all_trnas()
-    self.trna_fasta_files = compare.write_trnas_to_files(self.formset_data, self.seqs)
+    cls.formset_json_fh = NamedTemporaryFile('w', buffering = 1)
+    cls.formset_json = cls.formset_json_fh.name
+    cls.formset_data = [form.as_dict() for form in cls.formset]
+    cls.formset_json_fh.write(json.dumps(cls.formset_data))
+    cls.formset_json_fh.flush()
+
+    cls.factory = RequestFactory()
+    cls.request = cls.factory.get(reverse('explorer:bitchart', kwargs = {'formset_json': cls.formset_json}))
+    cls.seqs = compare.read_all_trnas()
+    cls.trna_fasta_files = compare.write_trnas_to_files(cls.formset_data, cls.seqs)
+    cls.ref_model_fh = compare.build_reference_model(cls.trna_fasta_files)
+    cls.ref_bits = compare.calculate_normalizing_scores(cls.ref_model_fh)
+
+    cls.bits = pd.DataFrame()
+    for i, trna_fasta_fh in enumerate(cls.trna_fasta_files[1:]):
+      current_bits = compare.align_trnas_collect_bit_scores(trna_fasta_fh.name, cls.formset_data[i + 2]['name'], cls.ref_model_fh.name)
+      cls.bits = cls.bits.append(current_bits)
+    cls.bits['score'] = round(cls.bits.apply(lambda x: x['score'] - cls.ref_bits[cls.ref_bits.position == x['position']]['score'].values[0], axis = 1), 2)
+
+    ref_taxid = cls.formset_data[0]['clade']
+    ref_isotype = cls.formset_data[0]['isotype']
+    ref_cons = compare.get_cons_bits(ref_taxid, ref_isotype)
+    ref_freqs = compare.get_modal_freqs(ref_taxid, ref_isotype)
+    cls.bits = pd.concat([cls.bits, ref_cons, ref_freqs], sort = True).reset_index(drop = True)
 
   def test_compare_read_all_trnas(self):
     self.assertTrue(len(self.seqs) > 0)
@@ -39,14 +65,55 @@ class CompareTests(TestCase):
         seqs = [seq for seq in SeqIO.parse(file.name, 'fasta')]
         self.assertTrue(len(seqs) > 0)
 
+  def test_compare_build_reference_model(self):
+    model = self.ref_model_fh.read()
+    self.assertIn(b'INFERNAL', model)
+    self.assertIn(b'CM', model)
+    self.assertIn(b'ROOT', model)
 
-  # def test_compare_build_reference_model(self):
-  #   pass
+  def test_compare_calculate_normalizing_scores(self):
+    self.assertIn('feature', self.ref_bits.columns)
+    self.assertEqual(self.ref_bits.shape, (67, 4))
+    self.assertEqual(len(self.ref_bits.seqname.unique()), 1)
+    self.assertIn('CMCONSENSUS', self.ref_bits.seqname.unique()[0])
 
-  # @tag('not-done')
-  # def test_compare_bitchart(self):
-  #   json_response = services.distribution(self.request, self.formset_json_filename)
-  #   plot_data = json.loads(json_response.content.decode('utf8'))
-  #   self.assertEqual(type(plot_data), dict)
-  #   self.assertTrue(len(plot_data) > 0)
+  def test_compare_align_trnas_collect_bit_scores(self):
+    bits = compare.align_trnas_collect_bit_scores(self.trna_fasta_files[0].name, self.formset_data[2]['name'], self.ref_model_fh.name)
+    self.assertEqual(bits.shape, (67, 5))
+    self.assertIn('total', bits.columns)
+    self.assertIn('group', bits.columns)
+    self.assertEqual(bits.group.unique()[0], 'Test')
+
+  def test_compare_get_cons_bits(self):
+    ref_cons = compare.get_cons_bits(self.ref_taxid, self.ref_isotype)
+    self.assertEqual(len(ref_cons.columns), 5)
+    self.assertEqual(ref_cons.group[0], 'Reference consensus')
+
+  def test_compare_get_modal_bits(self):
+    ref_freqs = compare.get_modal_freqs(self.ref_taxid, self.ref_isotype)
+    self.assertEqual(ref_freqs.shape, (132, 5))
+    self.assertEqual(ref_freqs.group[0], 'Most common feature')
+
+  def test_compare_check_bits_processed(self):
+    groups = sorted(self.bits.group.unique())
+    self.assertEqual(groups, ['Most common feature', 'Reference consensus', 'Test'])
+    try:
+      scores = self.bits.score.astype(float)
+      self.assertEqual(len(scores), len(self.bits.score))
+    except:
+      self.fail('Could not cast score to float')
+
+  def test_compare_format_bits_for_viz(self):
+    plot_data = compare.format_bits_for_viz(self.bits)
+    self.assertIn('bits', plot_data)
+    self.assertEqual(plot_data['groups'], ['Reference consensus', 'Most common feature', 'Test'])
+    bit_dict_keys = ['feature', 'group', 'label', 'position', 'score', 'total']
+    for i, row in plot_data['bits'].items():
+      self.assertEqual(sorted(row.keys()), bit_dict_keys)
+
+  def test_compare_bitchart(self):
+    # set temporary MEDIA_ROOT. test environment uses /tmp for tempfiles but production uses MEDIA_ROOT
+    with self.settings(MEDIA_ROOT = ''):
+      json_response = compare.bitchart(self.request, self.formset_json)
+      plot_data = json.loads(json_response.content.decode('utf8'))
 
