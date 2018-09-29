ac6c0374b4b619e9f5f3faaab61523a1e6970162
Brian Y Lin <me@brianl.in>
2018-09-28 21:03:56 -0700

    Dynamic tooltip positioning

--- explorer/static/explorer/js/compare.js
+++ explorer/static/explorer/js/compare.js
@@ -1,6 +1,6 @@
 var all_features = ['A', 'C', 'G', 'U', '-', 'A:U', 'U:A', 'G:C', 'C:G', 'G:U', 'U:G', 'A:A', 'A:C', 'A:G', 'C:A', 'C:C', 'C:U', 'G:A', 'G:G', 'U:C', 'U:U', '-:A', '-:C', '-:G', '-:U']
 var sorted_positions = ['1:72', '2:71', '3:70', '4:69', '5:68', '6:67', '7:66', '8', '9', '10:25', '11:24', '12:23', '13:22', '14', '15', '16', '17', '17a', '18', '19', '20', '20a', '20b', '21', '26', '27:43', '28:42', '29:41', '30:40', '31:39', '32', '33', '34', '35', '36', '37', '38', '44', '45', '46', '47', '48', '49:65', '50:64', '51:63', '52:62', '53:61', '54', '55', '56', '57', '58', '59', '60', '73']
-
+var adata;
 var draw_bitchart = function(plot_data) {
 	var bits = Object.values(plot_data['bits']);
 	var groups = plot_data['groups'];
@@ -20,7 +20,7 @@ var draw_bitchart = function(plot_data) {
 		.attr('width', bitchart_area_width)
 		.attr('height', bitchart_area_height)
 	
-  var tooltip = d3.select('.tooltip');
+  var tooltip = d3.select('.tooltip-compare');
   var tooltip_position = tooltip.select('#tooltip-position');
   var tooltip_group = tooltip.select('#tooltip-group');
   var tooltip_score = tooltip.select('#tooltip-score');
@@ -92,26 +92,37 @@ var draw_bitchart = function(plot_data) {
   	.attr('height', tile_width)
   	.style('fill', d => score_scale(d['score']))
   	.style('fill-opacity', d => alpha_scale(d['score']))
+    .attr('data-toggle', 'tooltip')
     .on('mouseover', function(d, i) {
       tooltip_position.html(d['position']);
       tooltip_group.html(d['group_name']);
       tooltip_score.html(d['score']);
       tooltip_feature.html(d['label']);
       tooltip_freq.html(d['total']);
-      tooltip.transition()
-        .duration(100)
-        .style('opacity', .9)
-        .style('left', d3.event.pageX + 'px')
-        .style('top', d3.event.pageY + 'px');
+      // tooltip.transition()
+      //   .duration(100)
+      //   .style('opacity', .9);
+      $('.tooltip-compare').css({
+        opacity: 0.9,
+      }).position({
+        my: "left top",
+        of: d3.event,
+        collision: "flip"
+      })
       d3.select(this)
         .transition()
         .duration(100)
         .attr('class', 'bitchart-rect-highlight');
     })
-    .on('mousemove', function(d, i) {  
-      tooltip.style('left', d3.event.pageX + 'px')
-        .style('top', d3.event.pageY + 'px');
+    .on('mousemove', function(d, i) {
+      $('.tooltip-compare').css({
+        opacity: 0.9,
+      }).position({
+        my: "left top",
+        of: d3.event,
+        collision: "flip"
       })
+    })
     .on('mouseout', function(d) {   
       tooltip.transition()    
         .duration(100)    
